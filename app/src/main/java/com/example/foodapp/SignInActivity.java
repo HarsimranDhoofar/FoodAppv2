3410090a4a1c9aaeedfa372c2b2698a47709ec7a
HarsimranDhoofar <52668529+HarsimranDhoofar@users.noreply.github.com>
2019-07-29 15:32:24 -0400

    Dashboard Created first time

--- app/src/main/java/com/example/foodapp/SignInActivity.java
+++ app/src/main/java/com/example/foodapp/SignInActivity.java
@@ -2,8 +2,12 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -14,6 +18,14 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
         mAuth = FirebaseAuth.getInstance();
+        FloatingActionButton signInbtn =findViewById(R.id.fbt_signIn_signIn);//Don't need to type casting in android studio 3
+
+        signInbtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInActivity.this, Dashboard.class);
+                startActivity(intent);
+            }
+        });
     }
     @Override
     public void onStart() {
--- app/src/main/java/com/example/foodapp/SignUpActivity.java
+++ app/src/main/java/com/example/foodapp/SignUpActivity.java
@@ -3,12 +3,9 @@ package com.example.foodapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -32,7 +29,7 @@ public class SignUpActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         mEmailField = findViewById(R.id.txt_signUp_username);
         mPasswordField = findViewById(R.id.txt_signUp_password);
-        signUpbtn = (FloatingActionButton) findViewById(R.id.fbt_signUp_signUp);//Don't need to type casting in android studio 3
+        signUpbtn = (FloatingActionButton) findViewById(R.id.fbt_signIn_signIn);//Don't need to type casting in android studio 3
 
         signUpbtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
