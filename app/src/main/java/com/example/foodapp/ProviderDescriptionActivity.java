440e6d5c15e826db19ca666229f00e1f84c4b422
HarsimranDhoofar <52668529+HarsimranDhoofar@users.noreply.github.com>
2019-11-10 15:38:04 -0500

    providerdescriptionactivity

--- app/src/main/java/com/example/foodapp/ProviderDescriptionActivity.java
+++ app/src/main/java/com/example/foodapp/ProviderDescriptionActivity.java
@@ -4,18 +4,29 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 
 public class ProviderDescriptionActivity extends AppCompatActivity {
 
+    ProvidersClass providersClass = new ProvidersClass();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provider_description);
+        FirebaseRetrive();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -25,6 +36,29 @@ public class ProviderDescriptionActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+    }
+    public void FirebaseRetrive(){
+        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference mReference = mDatabase.getReference("provider");
+
+
+        mReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    ProvidersClass providersClass = new ProvidersClass();
+                    providersClass.setProvidername(ds.child("name").getValue().toString());
+                    providersClass.setImgProvider(ds.child("img").getValue().toString());
+
+                }
+                System.out.println(providersClass.getProvidername());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
     }
